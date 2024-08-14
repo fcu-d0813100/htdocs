38a715da8a8880c4f422aa805fbbb906c99b0917
hugo09222 <eric6580256@yahoo.com.tw>
2024-08-14 23:15:22 +0800

    小專檔案初始化

---
+++ mid_project/header.php
@@ -0,0 +1,8 @@
+<header class="main-header d-flex justify-content-between bg-dark align-items-center fixed-top shadow ">
+    <a class="brand bg-black text-white text-decoration-none d-block " href="">後台管理系統</a>
+    <div class="d-flex align-items-center text-white text-md">
+      Hi, Admin!
+      <a class="btn btn-dark me-3" href="doLogout.php"><i class="fa-solid fa-right-from-bracket me-2 fa-fw"></i>Sign
+        Out</a>
+    </div>
+  </header>
---
+++ mid_project/sp/active.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Bootstrap Dashboard</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="noindex" />
+    <!-- theme stylesheet-->
+    <link
+      rel="stylesheet"
+      href="css/style.default.premium.css"
+      id="theme-stylesheet"
+    />
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css" />
+    <!-- font-awsome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+  </head>
+
+  <body>
+    <?php include("../header.php")?>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-3">
+        <?php include("../nav.php")?>
+      </div>
+        <div class="col-9">
+          <h1 class="h2 ">活動管理</h1>
+        </div>
+      </div>
+    </div>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/front.js"></script>
+    <script>
+      "use strict";
+      document.addEventListener("DOMContentLoaded", function () {
+        new Quill("#editor", {
+          modules: {
+            toolbar: [
+              ["bold", "italic", "underline", "strike"],
+              ["blockquote", "code-block"],
+              [{ header: 1 }, { header: 2 }],
+              [{ list: "ordered" }, { list: "bullet" }],
+              [{ indent: "-1" }, { indent: "+1" }],
+              [{ direction: "rtl" }],
+              [{ size: ["small", !1, "large", "huge"] }],
+              [{ header: [1, 2, 3, 4, 5, 6, !1] }],
+              [{ color: [] }, { background: [] }],
+              [{ font: [] }],
+              [{ align: [] }],
+              ["clean"],
+            ],
+          },
+          placeholder: "Compose an epic...",
+          theme: "snow",
+        });
+      });
+    </script>
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+      crossorigin="anonymous"
+    />
+  </body>
+</html>
